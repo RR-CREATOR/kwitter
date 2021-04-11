c4563a08c4491d0e8c26c25c42df0d243a243236
RR-CREATOR <77535915+RR-CREATOR@users.noreply.github.com>
2021-04-11 19:15:34 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,5 +1,21 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCpUPUXUT1VSxakRunFIEseVo3_DyeT8X8",
+    authDomain: "classtest-6b044.firebaseapp.com",
+    databaseURL: "https://classtest-6b044-default-rtdb.firebaseio.com",
+    projectId: "classtest-6b044",
+    storageBucket: "classtest-6b044.appspot.com",
+    messagingSenderId: "113708354409",
+    appId: "1:113708354409:web:a04e3270286869784f4bf8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 function addUser(){
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
     window.location = "kwitter_room.html";
+    firebase.database().ref("/").child(user_name).update({
+        purpose: "adding user"
+    });
+    sdocument.getElementById("user_name").value = "";
 }
